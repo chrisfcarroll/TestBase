be8c694456f76840a5a8e71b7b2b29b44156c401
Chris F Carroll <chris.carroll@moneyplus.com>
2013-10-07 17:34:29 +0100

    working on FakeHttpContext

--- TestBase-Mvc/FakeHttpContext.cs
+++ TestBase-Mvc/FakeHttpContext.cs
@@ -49,6 +49,10 @@ namespace TestBase
             //var httpRequest = new HttpRequest("", "http://localhost" + appVirtualDir + requestUrl, "");
             //var httpResponse = new HttpResponse(new StringWriter());
 
+            //
+            // Or look at http://stackoverflow.com/questions/705833/how-to-use-rhino-mocks-to-mock-an-httpcontext-application
+            //
+
             var wr = new SimpleWorkerRequest(appVirtualDir, "..", requestUrl, query, new StringWriter());
             var httpContext = new HttpContext(wr);
             httpContext.User = new WindowsPrincipal(WindowsIdentity.GetCurrent());
