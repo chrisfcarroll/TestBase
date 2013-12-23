7792973e508153a6af06acaa9ff90fcda8038382
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2013-12-23 17:19:46 +0000

    PackAndPush depends on NuGet in path. V 3.0.1.1

--- PackAndPush.cmd
+++ PackAndPush.cmd
@@ -1,23 +1,26 @@
+@Echo Off
 if .%1 == . Goto Usage
 pushd %~p0
 
-.nuget\NuGet.exe setApiKey %1
+NuGet.exe setApiKey %1
 
 cd TestBase
-..\.nuget\NuGet.exe Pack TestBase.csproj  -Symbols
+NuGet.exe Pack TestBase.csproj  -Symbols
 if errorlevel 1 goto Error
 
 cd ..\TestBase-Mvc
-..\.nuget\NuGet.exe Pack TestBase-Mvc.csproj -IncludeReferencedProjects 
+NuGet.exe Pack TestBase-Mvc.csproj -IncludeReferencedProjects 
 if errorlevel 1 goto Error
 
 popd
-.nuget\NuGet.exe push TestBase\TestBase.%2.nupkg
-.nuget\NuGet.exe push TestBase-Mvc\TestBase-Mvc.%2.nupkg
+NuGet.exe push TestBase\TestBase.%2.nupkg
+NuGet.exe push TestBase-Mvc\TestBase-Mvc.%2.nupkg
 
 goto :eof
 :Error
+echo.
 echo Aborted due to error whilst packing.
 popd
 :Usage
+Echo.
 Echo PackAndPush ^<NugetApiKey^> ^<VersionToPush^>
