78f99740d0be92a13875855373d48559a30bf00f
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2016-03-29 16:20:08 +0100

    MockHttpContext V3

--- TestBase.Tests/HttpContextTests/WhenMockingAndFakingHttpContextForAController.cs
+++ TestBase.Tests/HttpContextTests/WhenMockingAndFakingHttpContextForAController.cs
@@ -34,7 +34,7 @@ namespace TestBase.Tests.HttpContextTests
         }
 
         [Test]
-        public void Should_get_a_working_urlhelper()
+        public void Should_get_a_working_urlhelper__Given_no_routeConfig()
         {
             var uut = new FakeController().WithHttpContextAndRoutes();
             uut.Url.Action("a", "b").ShouldEqual("/b/a");
@@ -49,6 +49,16 @@ namespace TestBase.Tests.HttpContextTests
             uut.Url.Action("a", "c").ShouldEqual("/custom/c-a");
             uut.Url.Action("a", "c", new { id = 1, otherparameter = "2" }).ShouldEqual("/custom/c-a/1?otherparameter=2");
             uut.Url.Action("Index", "Home").ShouldEqual("/custom/Home-Index"); 
+        }
+
+        [Test]
+        public void Should_get_a_urlHelper_which_respects_appVirtualPath()
+        {
+            var appVirtualPath = "/OverriddenAppVirtualPath";
+            //
+            var uut = new FakeController().WithHttpContextAndRoutes(RegisterFakeRoutes, appVirtualPath:appVirtualPath);
+            //
+            uut.Url.Content("~/here.txt").ShouldBe(appVirtualPath + "/here.txt");
         }
 
         static void RegisterFakeRoutes(RouteCollection routes)
