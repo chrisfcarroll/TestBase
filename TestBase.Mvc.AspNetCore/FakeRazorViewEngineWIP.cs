cae0ab0724d04af15a3760f38878810e618bff9d
Chris.Carroll <chris.carroll@unforgettable.me.uk>
2018-05-22 19:56:09 +0100

    default message more corrections

---
+++ TestBase.Mvc.AspNetCore/FakeRazorViewEngineWIP.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace TestBase
+//{
+//    /// <summary>
+//    /// https://weblogs.asp.net/imranbaloch/custom-viewengine-aspnet5-mvc6
+//    /// services.AddMvc().Configure<MvcOptions>(options =>
+//    /// {
+//    ///  options.ViewEngines.Clear();
+//    ///  options.ViewEngines.Add(typeof(MyRazorViewEngine));
+//    /// });
+//    /// </summary>
+//    public class FakeRazorViewEngine : RazorViewEngine
+//    {
+//        public MyRazorViewEngine(IRazorPageFactory pageFactory,
+//            IRazorViewFactory viewFactory,
+//            IViewLocationExpanderProvider viewLocationExpanderProvider,
+//            IViewLocationCache viewLocationCache)
+//            : base(pageFactory,
+//                viewFactory,
+//                viewLocationExpanderProvider,
+//                viewLocationCache)
+//        {
+
+//        }
+	 
+//    }
+//}
