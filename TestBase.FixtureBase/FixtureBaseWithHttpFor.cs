84cc5d31ae265d9eb15535a05442ccdcb38ba1f2
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2018-11-18 17:20:58 +0000

    FixtureBases v1

--- TestBase.FixtureBase/FixtureBaseWithHttpFor.cs
+++ TestBase.FixtureBase/FixtureBaseWithHttpFor.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using ActivateAnything;
+using TestBase.AdoNet.FakeDb;
+using TestBase.HttpClient.Fake;
 
 namespace TestBase.FixtureBase
 {
@@ -7,30 +12,51 @@ namespace TestBase.FixtureBase
     ///     with <see cref="IActivateAnythingRule" />s to construct a UnitUnderTest.
     /// </summary>
     /// <typeparam name="T">The <see cref="System.Type" /> of the <see cref="UnitUnderTest" /></typeparam>
-    public class TestBaseFor<T>
-    {
-        protected internal T UnitUnderTest;
-        protected readonly AnythingActivator Activator;
-
-        protected TestBaseFor()
-        {
-            Activator = new AnythingActivator(this);
-            UnitUnderTest = Activator.New<T>();
-        }
-
-        protected TestBaseFor(params object[] useInstances)
-        {
-            var instances= new ActivateInstances(useInstances);
-            Activator= AnythingActivator.FromDefaultAndSearchAnchorRulesAnd(this, instances);
-        }
-    }
     public class FixtureBase
     {
+        object locker=new object();
+        bool activateIsStale = true;
+        AnythingActivator activator;
+
+        public ObservableCollection<object> Instances { get; } = new ObservableCollection<object>();
         
+        public AnythingActivator Activator
+        {
+            get
+            {
+                if (activateIsStale || activator==null)
+                {
+                    activator = AnythingActivator.FromDefaultAndSearchAnchorRulesAnd(this, new ActivateInstances(Instances));
+                    activateIsStale = false;
+                }
+                return activator;
+            }
+        }
+        protected FixtureBase() { Instances.CollectionChanged += (sender, args) => activateIsStale = true; }
     }
     
+    /// <inheritdoc cref="FixtureBase"/>
     public class FixtureBaseWithHttpFor<T> :FixtureBase
     {
+        public readonly FakeHttpClient HttpClient = new FakeHttpClient();
         
+        protected FixtureBaseWithHttpFor():base(){Instances.Add(HttpClient);}
+    }
+    
+    /// <inheritdoc cref="FixtureBase"/>
+    public class FixtureBaseWithDbFor<T> :FixtureBase
+    {
+        public readonly FakeDbConnection FakeDbConnection = new FakeDbConnection();
+        
+        protected FixtureBaseWithDbFor():base(){Instances.Add(FakeDbConnection);}
+    }
+    
+    /// <inheritdoc cref="FixtureBase"/>
+    public class FixtureBaseWithDbAndHttpFor<T> :FixtureBase
+    {
+        public readonly FakeDbConnection Db = new FakeDbConnection();
+        public readonly FakeHttpClient HttpClient = new FakeHttpClient();
+        
+        protected FixtureBaseWithDbAndHttpFor():base(){Instances.Add(Db);Instances.Add(HttpClient);}
     }
 }
