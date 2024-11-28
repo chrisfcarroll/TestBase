77a82eaebb8c3020f687ae2fe51359f449b0050e
Chris F Carroll <c.f.carroll@ljmu.ac.uk>
2024-11-28 17:57:58 +0000

    tests:ObjectTooString.cs:RegexReplaceKnownRuntimeVariableValues ignore more file expressions and Lengths

--- TooString/Extensions.cs
+++ TooString/Extensions.cs
@@ -62,17 +62,23 @@ public static class Extensions
     public static string? RegexReplaceKnownRuntimeVariableValues(this string? value) 
         => value?
             .ReplaceRegex("\"file:///[^\"]+\"","\"file:///--filename--\"")
+            .ReplaceRegex("\"file:///[BCD]:(/[^\",]+)+\",","\"file:///--filename--\",")
             .ReplaceRegex("\"[BCD]:\\[^\"]+\"","\"--filename--\"")
             .ReplaceRegex("\"[BCD]:\\\\[^\"]+\"","\"--filename--\"")
             .ReplaceRegex("\"(/[^/\"]+)+\"","\"--filename--\"")
             .ReplaceRegex("\"Value\":\\d+","\"Value\":9999999999")
+            .ReplaceRegex("\"Length\":\\d+","\"Length\":9999999999")
+            .ReplaceRegex("\"LongLength\":\\d+","\"LongLength\":9999999999")
             .ReplaceRegex("\"MetadataToken\":\\d+","\"MetadataToken\":100000000")
         
             .ReplaceRegex(" (/[^\" ,]+)+,"," --filename--,")
             .ReplaceRegex(" file:///[^\" ,]+,"," file:///--filename--,")
+            .ReplaceRegex(" file:///[BCD]:(/[^\",]+)+,"," file:///--filename--,")
             .ReplaceRegex(" [BCD]:\\[^,\"]+,"," --filename--,")
             .ReplaceRegex(" [BCD]:\\\\[^,\"]+,"," --filename--,")
             .ReplaceRegex(" Value = \\d+"," Value = 9999999999")
+            .ReplaceRegex(" Length = \\d+"," Length = 9999999999")
+            .ReplaceRegex(" LongLength = \\d+"," LongLength = 9999999999")
             .ReplaceRegex(" MetadataToken = \\d+"," MetadataToken = 100000000")
         
             .ReplaceRegex("[a-f0-9A-F\\-]{36}",System.Guid.Empty.ToString());
