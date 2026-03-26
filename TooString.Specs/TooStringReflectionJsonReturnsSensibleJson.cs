a321a0c22075487bc81d1cb9c9e2a49a70812ba9
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2026-03-26 21:04:12 +0000

    testcases:TooString*

--- TooString.Specs/TooStringReflectionJsonReturnsSensibleJson.cs
+++ TooString.Specs/TooStringReflectionJsonReturnsSensibleJson.cs
@@ -173,7 +173,7 @@ public class TooStringReflectionJsonReturnsSensibleJson
     {
         var value = new Circular{ A = "boo"};
         value.B = value;
-        var expected = "{\"A\":\"boo\",\"B\":{\"A\":\"boo\",\"B\":{\"A\":\"boo\",\"B\":\"TooString.Specs.Circular\"}}}";
+        var expected = "{\"A\":\"boo\",\"B\":{\"A\":\"boo\",\"B\":{\"A\":\"boo\",\"B\":\"TooString.Specs.Circular\",\"C\":null},\"C\":null},\"C\":null}";
         
         Assert.That(
             value.TooString(ReflectionStyle.Json),
