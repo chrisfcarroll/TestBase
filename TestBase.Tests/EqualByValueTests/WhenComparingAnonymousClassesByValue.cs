c17a79d67edff0f51a0ff416848170e36ace4ad4
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2026-03-27 12:37:58 +0000

    tweak:WhenComparingAnonymousClassesByValue.cs:make sure l and r aren't accidentally the same type.

--- TestBase.Tests/EqualByValueTests/WhenComparingAnonymousClassesByValue.cs
+++ TestBase.Tests/EqualByValueTests/WhenComparingAnonymousClassesByValue.cs
@@ -5,19 +5,18 @@ namespace TestBase.Tests.EqualByValueTests;
 [TestFixture]
 public class WhenComparingAnonymousClassesByValue
 {
+    object objectL = new {Id = 1, Name = "1"};
     [Test]
     public void Should_return_false_when_not_the_same()
     {
-            var objectL = new {Id = 1, Name = "1"};
             var objectR = new {Id = 1, Name = "2"};
             objectL.EqualsByValue(objectR).ShouldBeFalse();
-        }
+    }
 
     [Test]
     public void Should_return_true_when_the_same()
     {
-            var objectL = new {Id = 1, Name = "1"};
             var objectR = new {Id = 1, Name = "1"};
             objectL.EqualsByValue(objectR).ShouldBeTrue();
-        }
+    }
 }
