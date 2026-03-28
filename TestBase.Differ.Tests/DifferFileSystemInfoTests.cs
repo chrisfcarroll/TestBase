932f04a5cb1f5bd61f8349b5f54280cb32d28f04
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2026-03-28 21:51:41 +0000

    tests:DifferFileSystemInfoTests.cs

--- TestBase.Differ.Tests/DifferFileSystemInfoTests.cs
+++ TestBase.Differ.Tests/DifferFileSystemInfoTests.cs
@@ -9,8 +9,11 @@ public class DifferFileSystemInfoTests
     [Test]
     public void Equal_FileInfo_arrays()
     {
-        var list1 = new DirectoryInfo(".").GetFiles("*");
-        var list1Again = new DirectoryInfo(".").GetFiles("*");
+        var directoryInfo = new DirectoryInfo(".");
+        TestContext.Progress.WriteLine(directoryInfo.FullName);
+
+        var list1 = directoryInfo.GetFiles("*");
+        var list1Again = directoryInfo.GetFiles("*");
         Assume.That(list1.Length, Is.GreaterThan(0), "Test needs at least one file in cwd");
         Assert.That(Differ.Diff(list1, list1Again).AreEqual, Is.True);
     }
@@ -18,8 +21,11 @@ public class DifferFileSystemInfoTests
     [Test]
     public void Different_FileInfo_arrays()
     {
-        var list1 = new DirectoryInfo(".").GetFiles("*");
-        var list2 = new DirectoryInfo("..").GetFiles("*");
+        var directoryInfo = new DirectoryInfo(".");
+        TestContext.Progress.WriteLine(directoryInfo.FullName);
+
+        var list1 = directoryInfo.GetFiles("*");
+        var list2 = list1.Skip(1).Reverse().Take(5).ToArray();
         Assume.That(list1.Length, Is.GreaterThan(0));
         Assume.That(list2.Length, Is.GreaterThan(0));
         var result = Differ.Diff(list1, list2);
