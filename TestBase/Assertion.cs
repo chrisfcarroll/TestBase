d2f8ad5e03f4853a1db0462942b86ad9a0e7fe8f
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2026-04-19 21:53:30 +0100

    change:Assertion.cs:Stringify Actual as maxDepth=1,maxLength=3

--- TestBase/Assertion.cs
+++ TestBase/Assertion.cs
@@ -74,7 +74,7 @@ namespace TestBase
 #if NET6_0_OR_GREATER                    
                     { StringifyMethod.TooString, o => o.TooString(maxDepth: 1, maxLength: 3) },
 #else
-                    { StringifyMethod.TooString, o => o.TooString(maxDepth: 1, maxLength: 3) },
+                    { StringifyMethod.TooString, o => o.TooString() },
 #endif
                 };
 
@@ -182,7 +182,11 @@ namespace TestBase
                 AssertedDetail = assertionsExpression; 
                 Comment = string.Join(nl,
                     (comments ?? new List<(string, object)>())
-                    .Select(c => $"{c.Item1} : {c.Item2.TooString(maxDepth: 1, maxLength: 3)}"));
+#if NET6_0_OR_GREATER
+                    .Select(c => $"{c.Item1} : {c.Item2.TooString( maxDepth: 1, maxLength: 3 )}"));
+#else
+                    .Select(c => $"{c.Item1} : {c.Item2.TooString()}"));
+#endif
                 assertions(actual);
                 Result = true;
             }
