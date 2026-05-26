6dff7469f15be06d717c14608a21252dc90f2b4f
Chris Carroll <c.f.carroll@ljmu.ac.uk>
2026-05-26 15:41:58 +0100

    change:Inconclusive.cs:use CallerArgumentExpression on comment

--- TestBase/Inconclusive.cs
+++ TestBase/Inconclusive.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
 namespace TestBase
 {
@@ -37,11 +38,11 @@ namespace TestBase
         #else
         [DebuggerHidden]
         #endif
-        public static T IfPreconditionFails<T>(
-            T                         actual,
-            Expression<Func<T, bool>> predicate,
-            string                    comment = null,
-            params object[]           commentArgs)
+        public static T IfPreconditionFails<T>(T actual,
+                                               Expression<Func<T, bool>> predicate,
+                                               [CallerArgumentExpression("actual")]
+                                               string comment = null,
+                                               params object[] commentArgs)
         {
             var result = new Precondition<T>(actual, predicate, comment, commentArgs);
             if (result) return actual;
@@ -59,11 +60,11 @@ namespace TestBase
         #else
         [DebuggerHidden]
         #endif
-        public static T If<T>(
-            T                         actual,
-            Expression<Func<T, bool>> predicate,
-            string                    comment = null,
-            params object[]           commentArgs)
+        public static T If<T>(T actual,
+                              Expression<Func<T, bool>> predicate,
+                              [CallerArgumentExpression("actual")] 
+                              string comment = null,
+                              params object[] commentArgs)
         {
             return IfPreconditionFails(actual, predicate, comment, commentArgs);
         }
@@ -78,9 +79,9 @@ namespace TestBase
         [DebuggerHidden]
         #endif
         public static Precondition<BoolWithString> IfPreconditionFails(
-            BoolWithString  actual,
-            string          comment = null,
-            params object[] commentArgs)
+                            BoolWithString actual,
+                            [CallerArgumentExpression("actual")] string comment = null,
+                            params object[] commentArgs)
         {
             var result = new Precondition<BoolWithString>(actual, a => a, comment, commentArgs);
             if (result) return result;
@@ -97,10 +98,10 @@ namespace TestBase
         #else
         [DebuggerHidden]
         #endif
-        public static Precondition<BoolWithString> If(
-            BoolWithString  actual,
-            string          comment = null,
-            params object[] commentArgs)
+        public static Precondition<BoolWithString> If(BoolWithString  actual,
+                                                      [CallerArgumentExpression("actual")]
+                                                      string          comment = null,
+                                                      params object[] commentArgs)
         {
             return IfPreconditionFails(actual, comment, commentArgs);
         }
